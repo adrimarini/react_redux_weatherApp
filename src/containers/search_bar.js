6cbc5dc1bc4f3df91fb0d4070b31e51a9f63ba9c
Adrianna Marini <adrimarini>
2018-01-08 17:18:55 -0800

    made search input a controlled component and added binding context for this.onInputChange to update search input on value change

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    //initialize our state with this.state
+    //value of the input will be mapped to term
+    // and is blank string so its an empty input onload
+    this.state = { term: '' };
+
+    // bind with this lets the inputChange know which
+    // 'this' to refer to. At this level,'this' refers
+    // to the instance of SearchBar, so we are saying
+    // anywhere below where we refer to this.onInputChange
+    // replace that value with SearchBar instance of this
+    // by binding it
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    // need to setState to update the input form when
+    // user types into input b/c above its set to empty string
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       //className 'input-group' is bootstrap class; nothing special
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a 5 day forecase in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
